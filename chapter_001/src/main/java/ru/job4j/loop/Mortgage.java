694a81c023d63b702cf9016e95babf54d1bd322b
shalashkove <shalashkove@gmail.com>
2019-08-29 19:07:37 +0500

    5.6. Ипотека.[#171541]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int monthly, double percent) {
+        int year = 0;
+
+        double debt = amount;
+        while (debt > 0) {
+            if (debt * percent / 100 >= monthly * 12) {
+                year = Integer.MAX_VALUE;
+                break;
+            }
+            debt = debt * (100 + percent) / 100;
+            debt = debt - 12 * monthly;
+            year++;
+        }
+
+        return year;
+    }
+}
