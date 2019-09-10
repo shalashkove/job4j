d70f0569859453b23036dba0e42b43835ea95d73
shalashkove <shalashkove@gmail.com>
2019-09-10 21:14:39 +0500

    1.6. Петя и красная шапочка.[#171664]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryEat(Ball ball) {
+        System.out.println("Не могу съесть сам себя!");
+        System.out.println("... на самом деле в этом месте колобок убежал от бабки с дедкой.");
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/Fox.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Fox {
+    public void tryEat(Ball ball) {
+        System.out.println("Хотел колобок и лису обмануть как остальных.");
+        System.out.println("Но получилось наоборот - съела его лиса!");
+        System.out.println("Тут и сказочке конец.");
+    }
+}
