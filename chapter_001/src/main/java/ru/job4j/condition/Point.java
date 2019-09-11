b71b938057c0ccb948f7a17fd85adaaac73da320
shalashkove <shalashkove@gmail.com>
2019-09-11 06:19:12 +0500

    2.1. Рефакторинг - Расстояние между точками.[#171655]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,16 +1,35 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int y;
+
+    /**
+     * Конструтор, который принимает начальное состояние объекта "точка"
+     * @param first координата x
+     * @param second координата y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
     }
 
-    public static void main(String[] args) {
-        double result = distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        result = distance(-1, -1, 2, 5);
-        System.out.println("result (-1, -1) to (2, 5) " + result);
-        result = distance(2, 3, 3, 2);
-        System.out.println("result (2, 3) to (3, 2) " + result);
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,32 +1,33 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class PointTest {
-
     @Test
-    public void manWeight() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 3;
-        int y2 = 4;
-        double expected = 5;
-        double out = Point.distance(x1, y1, x2, y2);
-        final double delta = 1e-6;
-        Assert.assertEquals(expected, out, delta);
+    public void whenZeroAndTenThenTen() {
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 10);
+        double result = first.distance(second);
+        first.info();
+        second.info();
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(10D));
     }
 
     @Test
-    public void manWeight2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 2;
-        double expected = 2.828427;
-        double out = Point.distance(x1, y1, x2, y2);
-        final double delta = 1e-6;
-        Assert.assertEquals(expected, out, delta);
+    public void whenCheckItself() {
+        Point point = new Point(0, 0);
+        double result = point.distance(point);
+        assertThat(result, is(0D));
     }
 
+    @Test
+    public void whenShowInfo() {
+        Point first = new Point(1, 1);
+        first.info();
+        Point second = new Point(2, 2);
+        second.info();
+    }
 }
