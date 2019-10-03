478624f6baf73364654fb5d6d36532215483cc2b
shalashkove <shalashkove@gmail.com>
2019-10-03 11:29:35 +0500

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#171639]

--- chapter_002/src/main/java/ru/job4j/tracker/FindByNameAction.java
+++ chapter_002/src/main/java/ru/job4j/tracker/FindByNameAction.java
@@ -8,14 +8,11 @@ public class FindByNameAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.print("Enter Item name please: ");
         String name = input.askStr("");
         Item[] founded = tracker.findByName(name);
         if (founded != null) {
-            System.out.println(String.format("Item('s) with name = %s was successfully found", name));
             for (int i = 0; i != founded.length; i++) {
-                System.out.println(String.format("Item № %d, id = %s, name = %s", i + 1,
-                        founded[i].getId(), founded[i].getName()));
+                System.out.println(String.format("%s %s", founded[i].getId(), founded[i].getName()));
             }
         } else {
             System.out.println(String.format("Item with name = %s was not found", name));
