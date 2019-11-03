75075074da87fa11cde0d9c83fdded9e6897d9f9
shalashkove <shalashkove@gmail.com>
2019-11-04 04:34:54 +0500

    1. Телефонный справочник на базе ArrayList[#171611]

---
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,43 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneDictionary {
+    private List<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (person == null) {
+                continue;
+            }
+            if (person.getName() != null && person.getName().contains(key)) {
+                result.add(person);
+                continue;
+            }
+            if (person.getSurname() != null && person.getSurname().contains(key)) {
+                result.add(person);
+                continue;
+            }
+            if (person.getPhone() != null && person.getPhone().contains(key)) {
+                result.add(person);
+                continue;
+            }
+            if (person.getAddress() != null && person.getAddress().contains(key)) {
+                result.add(person);
+                continue;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("Petr");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("yans");
+        assertThat(persons.iterator().next().getAddress(), is("Bryansk"));
+    }
+
+    @Test
+    public void whenNoFinding() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("nothing");
+        assertThat(persons.size(), is(0));
+    }
+}
