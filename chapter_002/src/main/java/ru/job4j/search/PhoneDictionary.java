20575455843a621777e97e8c56511e182667ce5a
shalashkove <shalashkove@gmail.com>
2019-11-05 06:36:22 +0500

    1. Телефонный справочник на базе ArrayList[#171611]

--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -18,24 +18,16 @@ public class PhoneDictionary {
     public List<Person> find(String key) {
         List<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person == null) {
-                continue;
-            }
-            if (person.getName() != null && person.getName().contains(key)) {
-                result.add(person);
-                continue;
-            }
-            if (person.getSurname() != null && person.getSurname().contains(key)) {
-                result.add(person);
-                continue;
-            }
-            if (person.getPhone() != null && person.getPhone().contains(key)) {
-                result.add(person);
-                continue;
-            }
-            if (person.getAddress() != null && person.getAddress().contains(key)) {
-                result.add(person);
-                continue;
+            if (person != null) {
+                if (person.getName() != null && person.getName().contains(key)) {
+                    result.add(person);
+                } else if (person.getSurname() != null && person.getSurname().contains(key)) {
+                    result.add(person);
+                } else if (person.getPhone() != null && person.getPhone().contains(key)) {
+                    result.add(person);
+                } else if (person.getAddress() != null && person.getAddress().contains(key)) {
+                    result.add(person);
+                }
             }
         }
         return result;
---
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,21 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определять по полю приоритет.
+     * Для вставик использовать add(int index, E value)
+     * @param task задача
+     */
+    public void put(Task task) {
+        //TODO добавить вставку в связанный список.
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
