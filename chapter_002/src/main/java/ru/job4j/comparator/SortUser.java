655d09b4f8529721886c67166d5ab9276599d28b
shalashkove <shalashkove@gmail.com>
2019-12-11 06:27:32 +0500

    2. Сортировка User с использованием Comparator[#171604]

--- chapter_002/src/main/java/ru/job4j/comparator/SortUser.java
+++ chapter_002/src/main/java/ru/job4j/comparator/SortUser.java
@@ -1,15 +1,38 @@
 package ru.job4j.comparator;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SortUser {
 
     public Set<User> sort(List<User> list) {
         Set<User> result = new TreeSet<>();
         result.addAll(list);
+        return result;
+    }
+
+    public List<User> sortNameLength(List<User> list) {
+        List<User> result = list;
+        Collections.sort(result, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().length() - o2.getName().length();
+            }
+        });
+        return result;
+    }
+
+    public List<User> sortByAllFields(List<User> list) {
+        List<User> result = list;
+        Collections.sort(result, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int result = o1.getName().compareTo(o2.getName());
+                if (result == 0) {
+                    result = o1.getAge() - o2.getAge();
+                }
+                return result;
+            }
+        });
         return result;
     }
 }
--- chapter_002/src/test/java/ru/job4j/comparator/SortUserTest.java
+++ chapter_002/src/test/java/ru/job4j/comparator/SortUserTest.java
@@ -5,10 +5,7 @@ import ru.job4j.tracker.Item;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.StringJoiner;
-import java.util.TreeSet;
+import java.util.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -16,13 +13,36 @@ import static org.junit.Assert.*;
 public class SortUserTest {
 
     @Test
-    public void whenFindByIdInEmptyItems() {
+    public void whenSimpleUserSort() {
         SortUser sortUser = new SortUser();
         User one = new User("Eva", 30);
         User two = new User("Adam", 20);
         User three = new User("third", 10);
         Set<User> expect = new TreeSet<>(Arrays.asList(three, two, one));
         Set<User> result = sortUser.sort(Arrays.asList(one, three, two));
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortByNameLength() {
+        SortUser sortUser = new SortUser();
+        User one = new User("Eva", 30);
+        User two = new User("Adam", 20);
+        User three = new User("third", 10);
+        List<User> expect = new ArrayList<>(Arrays.asList(one, two, three));
+        List<User> result = sortUser.sortNameLength(Arrays.asList(two, three, one));
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortByAllFields() {
+        SortUser sortUser = new SortUser();
+        User one = new User("Сергей", 25);
+        User two = new User("Иван", 30);
+        User three = new User("Сергей", 20);
+        User fouth = new User("Иван", 25);
+        List<User> expect = new ArrayList<>(Arrays.asList(fouth, two, three, one));
+        List<User> result = sortUser.sortByAllFields(new ArrayList<>(Arrays.asList(one, two, three, fouth)));
         assertThat(result, is(expect));
     }
 }
