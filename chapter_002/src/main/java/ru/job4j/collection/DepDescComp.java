81caead60d2bc85631e8e2faa8853c9ecd5e61ac
shalashkove <shalashkove@gmail.com>
2020-02-10 13:07:40 +0500

    Контрольные вопросы.[#171609]

--- chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -6,26 +6,18 @@ public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
         int result = 0;
-        List<String> o1ArrayList = Arrays.asList(o1.split("/"));
-        List<String> o2ArrayList = Arrays.asList(o2.split("/"));
+        char[] o1Array = o1.toCharArray();
+        char[] o2Array = o2.toCharArray();
         int index = 0;
-        while (true) {
-            if (index >= o1ArrayList.size() & index >= o2ArrayList.size()) {
-                break;
-            }
-            if (index >= o1ArrayList.size() & index < o2ArrayList.size()) {
-                result = 1;
-                break;
-            }
-            if (index < o1ArrayList.size() & index >= o2ArrayList.size()) {
-                result = -1;
-                break;
-            }
-            result = o1ArrayList.get(index).compareTo(o2ArrayList.get(index));
+        while (index < o1Array.length && index < o2Array.length) {
+            result = o1Array[index] - o2Array[index];
             if (result != 0) {
                 break;
             }
             index++;
+        }
+        if (result == 0 & o1Array.length != o2Array.length) {
+            result = o2Array.length - o1Array.length;
         }
         return result;
     }
