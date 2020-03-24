12a4baa8137ca90169bcb858a956e47bfd94b30a
shalashkove <shalashkove@gmail.com>
2020-03-24 17:26:32 +0500

    3. Уникальность элементов и сортировка.[#171500]

--- chapter_002/src/test/java/ru/job4j/stream/ProfilesTest.java
+++ chapter_002/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -3,6 +3,7 @@ package ru.job4j.stream;
 import org.junit.Test;
 import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -20,6 +21,27 @@ public class ProfilesTest {
         List<Address> expected = List.of(
                 new Address("Moscow", "Lenin st.", 10, 20),
                 new Address("London", "Picadili st.", 5, 120),
+                new Address("Paris", "Arbat", 20, 50)
+        );
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenProfilesThenUniqueAddresses() {
+        Profiles profiles = new Profiles();
+        List<Address> result = profiles.collect(List.of(
+                new Profile(new Address("Moscow", "Lenin st.", 10, 20)),
+                new Profile(new Address("Moscow", "Lenin st.", 10, 20)),
+                new Profile(new Address("London", "Picadili st.", 5, 120)),
+                new Profile(new Address("London", "Picadili st.", 5, 120)),
+                new Profile(new Address("London", "Picadili st.", 5, 120)),
+                new Profile(new Address("Paris", "Arbat", 20, 50))
+        ));
+        Comparator<Address> comparator = (o1, o2) -> o1.getCity().compareTo(o2.getCity());
+        result = result.stream().sorted(comparator).distinct().collect(Collectors.toList());
+        List<Address> expected = List.of(
+                new Address("London", "Picadili st.", 5, 120),
+                new Address("Moscow", "Lenin st.", 10, 20),
                 new Address("Paris", "Arbat", 20, 50)
         );
         assertThat(result, is(expected));
