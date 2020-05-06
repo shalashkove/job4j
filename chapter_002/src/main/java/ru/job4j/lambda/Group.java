12c9b2701c92407432765326f089339c11247ee6
shalashkove <shalashkove@gmail.com>
2020-05-06 22:18:28 +0500

    Группировка по интересам. [#106993]

--- chapter_002/src/main/java/ru/job4j/lambda/Group.java
+++ chapter_002/src/main/java/ru/job4j/lambda/Group.java
@@ -10,23 +10,19 @@ import java.util.stream.Stream;
 public class Group {
 
     public static Map<String, Set<String>> sections(List<Student> students) {
-        Map<String, Set<String>> result = students.stream().flatMap(
+        return students.stream().flatMap(
                 // собираем объект Holder с unit и name
                 e -> e.getUnits().stream().map(m -> new Holder(m, e.getName()))
         )
                 .collect(// собираем карту
-                //Collectors.groupingBy(t -> t.key, // определяем группировку
-                Collectors.groupingBy(Group.Holder::getKey, // определяем группировку
-//                        Collector.of(
-//                                //HashSet::new, // аккумулятор.
-//                                ??? new HashSet<String>(),
-//                                (set, el) -> // как добавлять данные.
-//                                        (left, right) -> { left.addAll(right); return left; } // для агрегации.
-//                        )
-                        Collectors.mapping(Holder::getValue, Collectors.toSet())
-                )
-        );
-        return result;
+                Collectors.groupingBy(t -> t.key, // определяем группировку
+                                Collector.of(
+                                        HashSet::new, // аккумулятор.
+                                        (set, element) -> set.add(element.value),  // как добавлять данные.
+                                                (left, right) -> { left.addAll(right); return left; } // для агрегации.
+                                )
+                        )
+                );
     }
 
     static class Holder {
@@ -35,22 +31,6 @@ public class Group {
         Holder(String key, String value) {
             this.key = key;
             this.value = value;
-        }
-
-        public String getKey() {
-            return key;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        @Override
-        public String toString() {
-            return "Holder{"
-                    + "key='" + key + '\''
-                    + ", value='" + value + '\''
-                    + '}';
         }
     }
 }
